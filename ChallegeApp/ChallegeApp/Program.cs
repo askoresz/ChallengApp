f426675437e7b8d080691d58fd0502adbc451160
Andrzej <144821096+askoresz@users.noreply.github.com>
2023-10-26 12:02:13 +0200

    Stworzenie aplikacji

--- ChallegeApp/ChallegeApp/Program.cs
+++ ChallegeApp/ChallegeApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Andrzej!");
+﻿User user1 = new User("Adam", "32423fddf");
+User user2 = new User("Monika", "32423fddf");
+User user3 = new User("Andrzej", "32423fddf");
+User user4 = new User("Sylwestr", "32423fddf");
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
+var name = User.GameName;
+var pi = Math.PI;
+class User
+
+{
+
+    public static string GameName = "Diablo";
+    private List<int> score = new List<int>();
+
+
+
+    public User(string Login, string password)
+    {
+        this.Login = Login;
+        this.Password = password;
+
+    }
+    public string Login { get; private set; }
+
+    public string Password { get; private set; }
+
+    public int Result
+    {
+        get
+        {
+            return this.score.Sum();
+        }
+    }
+    public void AddScore(int number)
+    {
+        this.score. Add(number);
+    }
+
+}
---
+++ ChallegeApp/ChallegeApp/User.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace ChallegeApp
+{
+    public class Employee
+    {
+         private List<int> score = new List<int>();
+            public Employee (string name, string surname,int age)
+            {
+                this.Name = name;
+                this.Surname = surname;
+                this.Age = age;
+
+            }
+            public string Name { get; private set; }
+
+            public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+            public int Result
+            {
+                get
+                {
+                    return this.score.Sum();
+                }
+            }
+            public void AddScore(int number)
+            {
+                this.score.Add(number);
+            }
+
+        }
+    }
+
